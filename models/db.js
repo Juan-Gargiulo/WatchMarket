33b9b05da2a17569310a6b1d6ec3ff7452bab364
facundo vega <facuvega1989@gmail.com>
2018-02-24 12:17:03 -0300

    testing env vars on heroku

--- models/db.js
+++ models/db.js
@@ -1,4 +1,6 @@
 var mongoose = require('mongoose');
 var url ="mongodb://admin:1234@ds245548.mlab.com:45548/watchmarket";
+console.log(NODE_ENV);
+console.log(process.env.MONGODB_URI);
 mongoose.connect(url);
 // si lo igualo a la variable de entorno , me tira un error que me dice que "url debe ser un string , hay q buscarle la vuelta a esa gilada"
