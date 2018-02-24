fb2bdd9652ebc5060fec7fab5eb8c0a861af677d
juan <juanpab84@gmail.com>
2018-02-24 14:42:00 -0300

    add cloudinary image upload

---
+++ client/src/Containers/Forms/Mallas/index.js
@@ -0,0 +1,51 @@
+import React, {Component} from 'react'
+
+
+import axios, { post, put } from 'axios';
+
+class SimpleReactFileUpload extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state ={
+      file:null
+    }
+    this.onFormSubmit = this.onFormSubmit.bind(this)
+    this.onChange = this.onChange.bind(this)
+    this.fileUpload = this.fileUpload.bind(this)
+  }
+
+  onFormSubmit(e){
+    e.preventDefault() // Stop form submit
+    this.fileUpload(this.state.file).then((response)=>{
+      console.log(response.data);
+    })
+  }
+
+  onChange(e) {
+    this.setState({file:e.target.files[0]})
+  }
+  fileUpload(file){
+    const url = 'api/mallas';
+    const formData = new FormData();
+    formData.append('images',file)
+    const config = {
+        headers: {
+            'content-type': 'multipart/form-data'
+        }
+    }
+    return  post(url, formData,config)
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.onFormSubmit}>
+        <h1>File Upload</h1>
+        <input type="file" name="images" onChange={this.onChange} />
+        <button type="submit">Upload</button>
+      </form>
+   )
+  }
+}
+
+export default SimpleReactFileUpload
--- client/src/Containers/Routes/index.js
+++ client/src/Containers/Routes/index.js
@@ -7,14 +7,15 @@ import {
 } from 'react-router-dom'
 
 import Gallery from '../Gallery'
-import Detail from '../Detail';
-import TableTest from '../../Components/TableTest'
+import Detail from '../Detail'
+import FormMallas from '../Forms/Mallas'
 
 import { Container } from './style.js'
 
 export const paths = {
   GALLERY: '/',
   CARD_DETAIL: '/detail/:id',
+  FORM_MALLAS: '/mallas'
 };
 
 const routes = [
@@ -28,8 +29,8 @@ const routes = [
     render: props => <Detail title="Detail" {...props} />
   },
   {
-    path: '/table',
-    render: props => <TableTest title="Table" {...props} />
+    path: paths.FORM,
+    render: props => <FormMallas title="Table" {...props} />
   },
   {
     component: () => <h3>No match for this route></h3>
