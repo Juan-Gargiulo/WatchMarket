053f6c3a84bd8f51f4dc0c1594db9b531eb98f46
juan <juanpab84@gmail.com>
2018-03-02 19:07:33 -0300

    WIP form mallas

--- client/src/Containers/Forms/Mallas/index.js
+++ client/src/Containers/Forms/Mallas/index.js
@@ -1,19 +1,49 @@
 import React, {Component} from 'react'
 
+import SelectField from 'material-ui/SelectField';
+import Slider from 'material-ui/Slider';
+import MenuItem from 'material-ui/MenuItem';
+
+import { Row, Col } from 'react-grid-system';
+
+
+import Divider from 'material-ui/Divider';
+import Paper from 'material-ui/Paper';
 
 import axios, { post, put } from 'axios';
+
+const styles = {
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+    width: "80%"
+  },
+
+};
 
 class SimpleReactFileUpload extends React.Component {
 
   constructor(props) {
     super(props);
     this.state ={
-      file:null
+      file:null,
+      filtros: {
+        tipo: "",
+        sssss: ""
+      } 
     }
     this.onFormSubmit = this.onFormSubmit.bind(this)
     this.onChange = this.onChange.bind(this)
     this.fileUpload = this.fileUpload.bind(this)
   }
+
+  handleChange = tipo => (event, index, value) => {
+    console.log(tipo)
+    const filtros = this.state.filtros
+    filtros[tipo] = value
+    this.setState({filtros})
+  };
 
   onFormSubmit(e){
     e.preventDefault() // Stop form submit
@@ -29,20 +59,45 @@ class SimpleReactFileUpload extends React.Component {
     const url = 'api/mallas';
     const formData = new FormData();
     formData.append('images',file)
+
     const config = {
         headers: {
             'content-type': 'multipart/form-data'
         }
     }
-    return  post(url, formData,config)
+    return post(url, formData,config)
   }
 
   render() {
     return (
       <form onSubmit={this.onFormSubmit}>
-        <h1>File Upload</h1>
-        <input type="file" name="images" onChange={this.onChange} />
-        <button type="submit">Upload</button>
+        
+        <Row>
+        <Col xs={12} debug align="center">
+          <Col xs={4} debug align="center">
+              <h1>File Upload</h1>
+              <input type="file" name="images" onChange={this.onChange} />
+              <button type="submit">Upload</button>
+              <SelectField
+                floatingLabelText="Frequency"
+                value={this.state.value}
+                onChange={this.handleChange("tipo")}
+              >
+                <MenuItem value={"larga"} primaryText="Larga" />
+                <MenuItem value={"corta"} primaryText="Corta" />
+              </SelectField>
+              <SelectField
+                floatingLabelText="sssss"
+                value={this.state.value}
+                onChange={this.handleChange("sssss")}
+              >
+                <MenuItem value={"aa"} primaryText="aa" />
+                <MenuItem value={"bb"} primaryText="bb" />
+              </SelectField>
+              <Slider min={10} max={30}/>
+          </Col>
+          </Col>
+        </Row>
       </form>
    )
   }
